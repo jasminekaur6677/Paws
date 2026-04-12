df09bc992c1c4d149ef87a3c877e84ddc89d19b9
Jasmine <sandhujasmine@seasiainfotech.com>
2026-04-12 16:01:49 +0530

    project setup

---
+++ Paws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paws
+//
+//  Created by Jasmine Kaur on 12/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Jasmine Kaur on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
